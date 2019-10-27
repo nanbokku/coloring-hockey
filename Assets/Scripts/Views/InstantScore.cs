670c7eeecfb661051f99a61c749bf2a536a17d9c
nanbokku <29055086+nanbokku@users.noreply.github.com>
2019-10-27 23:21:52 +0900

    [add] Add InstantScore

---
+++ Assets/Scripts/Views/InstantScore.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.Events;
+
+public class InstantScore : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI playerScoreTxt = null;
+    [SerializeField]
+    private TextMeshProUGUI enemyScoreTxt = null;
+    [SerializeField]
+    private Animator instantScoreAnimator = null;
+    [SerializeField]
+    private Color defaultColor = Color.white;
+    [SerializeField]
+    private Color addedScoreColor = default;
+
+    public UnityAction OnScoreAnimFinished { get; set; } = null;
+
+    private const string PlayerScoreAnimTriggerStr = "flash_left";
+    private const string EnemyScoreAnimTriggerStr = "flash_right";
+
+    void Awake()
+    {
+        playerScoreTxt.text = 0.ToString();
+        enemyScoreTxt.text = 0.ToString();
+    }
+
+    public void UpdatePlayerScore(int score)
+    {
+        playerScoreTxt.text = score.ToString();
+        playerScoreTxt.color = addedScoreColor;
+        enemyScoreTxt.color = defaultColor;
+        instantScoreAnimator.SetTrigger(PlayerScoreAnimTriggerStr);
+    }
+
+    public void UpdateEnemyScore(int score)
+    {
+        enemyScoreTxt.text = score.ToString();
+        enemyScoreTxt.color = addedScoreColor;
+        playerScoreTxt.color = defaultColor;
+        instantScoreAnimator.SetTrigger(EnemyScoreAnimTriggerStr);
+    }
+
+    public void ScoreAnimFinished()
+    {
+        OnScoreAnimFinished();
+    }
+}
