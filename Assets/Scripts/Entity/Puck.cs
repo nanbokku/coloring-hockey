df39278c5a6edcac977469dc48fd8d0c4cf2c0ab
nanbokku <29055086+nanbokku@users.noreply.github.com>
2019-11-02 00:52:39 +0900

    [fix] Fix the bug of goal judgement

--- Assets/Scripts/Entity/Puck.cs
+++ Assets/Scripts/Entity/Puck.cs
@@ -4,18 +4,44 @@ public class Puck : MonoBehaviour
 {
     [SerializeField]
     private float restitution = 0;  // TODO: Wallの変数にするべき
+    [SerializeField]
+    private SphereCollider sphereCollider = null;
 
     // TODO: rigidbodyで動きを実装するとすり抜けがはげしかったりする
 
+    private Vector3 lastPosition = Vector3.zero;
+
     private float velocity = 0;
 
-    public void SetVelocity(float velocity)
+    void Start()
     {
-        this.velocity = velocity;
+        lastPosition = this.transform.position;
     }
 
-    void OnCollisionEnter(Collision other)
-    {
-        velocity = restitution * velocity;
-    }
+    // void FixedUpdate()
+    // {
+    //     // 前フレームの位置から現在位置間で衝突するか調べる
+    //     Ray ray = new Ray(lastPosition, this.transform.position);
+
+    //     RaycastHit hit;
+    //     bool isHit = Physics.SphereCast(ray.origin, sphereCollider.radius * this.transform.localScale.x, ray.direction, out hit, Vector3.Distance(lastPosition, this.transform.position));
+
+    //     if (isHit)
+    //     {
+    //         // 衝突を検知したときは，位置を衝突前に戻す
+    //         this.transform.position = hit.point - ray.direction * sphereCollider.radius * this.transform.localScale.x;
+    //     }
+
+    //     lastPosition = this.transform.position;
+    // }
+
+    // public void SetVelocity(float velocity)
+    // {
+    //     this.velocity = velocity;
+    // }
+
+    // void OnCollisionEnter(Collision other)
+    // {
+    //     velocity = restitution * velocity;
+    // }
 }
--- Assets/Scripts/GoalArea.cs
+++ Assets/Scripts/GoalArea.cs
@@ -6,12 +6,20 @@ public class GoalArea : MonoBehaviour
 {
     [SerializeField]
     private PlayerType score = PlayerType.Human;
+    [SerializeField]
+    private Vector3 goalDirection = Vector3.zero;
 
     public UnityAction<PlayerType> OnGoaled { get; set; } = null;
 
     void OnTriggerExit(Collider other)
     {
         if (other.gameObject.tag != StageData.TagNameOfPuck) return;
+
+        Rigidbody rb = other.GetComponent<Rigidbody>();
+        Vector3 direction = rb.velocity.normalized;
+
+        // ゴール方向にpuckが抜けていかないときは無視
+        if (Vector3.Angle(direction, goalDirection) > 90f) return;
 
         if (OnGoaled != null) OnGoaled(score);
     }
